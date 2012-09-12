4d3d6aeb1b767b014a785ab04b3ba0da7a302e3b
Jackson <jackpinto@gmail.com>
2012-09-12 08:11:14 -0300

    Implementação do Mapeamento para Pessoa, PessoaFisica, PessoaJuridica

---
+++ DominioSimplificado/Mapeamento/PessoaJuridicaMap.cs
@@ -0,0 +1,18 @@
+﻿using Dominio.Simples.Model;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Dominio.Simples.Mapeamento
+{
+    public class PessoaJuridicaMap : SubclassMapping<PessoaJuridica>
+    {
+        public PessoaJuridicaMap()
+        {
+            DiscriminatorValue("J");
+            Property(pessoa => pessoa.Cnpj, map => map.Column("CNPJ"));
+            Property(pessoa => pessoa.DataAbertura, map =>map.Column("DATAABERTURA"));
+            Property(pessoa => pessoa.NomeFantasia, map => map.Column("NOMEFANTASIA"));
+            Property(pessoa => pessoa.RazaoSocial, map => map.Column("RAZAOSOCIAL"));
+        }
+    }
+}
